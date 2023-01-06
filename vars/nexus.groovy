40fc9351ea9c4c47abcd275b7186b5eb99d48454
KrishnaPrasad K S <krishnaprasad1287@gmail.com>
2023-01-06 17:27:00 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -8,19 +8,35 @@ def jfUpload(String url, String access_token, String src, String dest){
 
 def nexus(String artifactId, String classifier, String file, String type, String credentialsId, String groupId, String nexusUrl, String nexusVersion, String protocol, String repository, String version)
 {
-nexusArtifactUploader artifacts: [
-                    [
-                      artifactId: ${artifactId},
-                      classifier: ${classifier},
-                      file: ${file},
-                      type: ${type}
-                     ]
-                ],
-                credentialsId: ${credentialsId}, 
-                groupId: ${groupId}, 
-                nexusUrl: ${nexusUrl}, 
-                nexusVersion: ${nexusVersion}, 
-                protocol: ${protocol}, 
-                repository: ${repository}, 
-                version: ${version}
+// nexusArtifactUploader artifacts: [
+//                     [
+//                       artifactId: ${artifactId},
+//                       classifier: ${classifier},
+//                       file: ${file},
+//                       type: ${type}
+//                      ]
+//                 ],
+//                 credentialsId: ${credentialsId}, 
+//                 groupId: ${groupId}, 
+//                 nexusUrl: ${nexusUrl}, 
+//                 nexusVersion: ${nexusVersion}, 
+//                 protocol: ${protocol}, 
+//                 repository: ${repository}, 
+//                 version: ${version}
+// }
+         nexusArtifactUploader artifacts: [
+                     [
+                      artifactId: 'spring-boot-thymeleaf',
+                      classifier: '',
+                      file: '/var/lib/jenkins/.m2/repository/pl/codeleak/demos/sbt/spring-boot-thymeleaf/2.0.0/spring-boot-thymeleaf-2.0.0.war',
+                      type: 'war'
+                      ]
+                 ],
+                 credentialsId: 'nexus', 
+                 groupId: 'pl.codeleak.demos.sbt', 
+                 nexusUrl: '13.233.100.229:8081', 
+                 nexusVersion: 'nexus3', 
+                 protocol: 'http', 
+                 repository: 'ansible', 
+                 version: '2.0.0'
 }
