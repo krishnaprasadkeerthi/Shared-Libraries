0e4ba741d9277dcf1c20def955a7e56d3a609fe6
KrishnaPrasad K S <krishnaprasad1287@gmail.com>
2023-01-06 14:21:06 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -5,3 +5,6 @@ def jfPing(String url){
 def jfUpload(String url, String access_token, String src, String dest){
   sh "jf rt u --url ${url} --access-token ${access_token} ${src} ${dest}"
 }
+
+def nexus(String credentialsId, String groupId, String nexusUrl, String nexusVersion, String protocol, String repository, String version) {
+}
