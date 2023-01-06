5e079689915d1d60c562b44bc07075054773b537
KrishnaPrasad K S <krishnaprasad1287@gmail.com>
2023-01-06 19:36:04 +0530

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -3,5 +3,9 @@
 // }
 
 def sonarAnalysis(String Project_Key, String Project_Name, String Sonar_URL, String Sonar_login){
-      sh "mvn sonar:sonar -Dsonar.projectKey=${Project_Key}	-Dsonar.projectName=${Project_Name} -Dsonar.host.url=${Sonar_URL}  -Dsonar.login=${Sonar_login}"
+      sh "mvn sonar:sonar \
+      -Dsonar.projectKey=${Project_Key}	\
+      -Dsonar.projectName=${Project_Name}  \
+      -Dsonar.host.url=${Sonar_URL}  \
+      -Dsonar.login=${Sonar_login}"
 }
