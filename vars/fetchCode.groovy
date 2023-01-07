4af88a00384dc1a9135c18915e5b9f98ad84febb
KrishnaPrasad K S <krishnaprasad1287@gmail.com>
2023-01-07 10:50:06 +0530

    Update fetchCode.groovy

--- vars/fetchCode.groovy
+++ vars/fetchCode.groovy
@@ -6,3 +6,7 @@ def fetchCode(){
 def GitFetch(){
  git url: "https://github.com/krishnaprasadkeerthi/Ansible-Demo.git"
 }
+
+def fetchCode1(){
+  git url: "https://github.com/krishnaprasadkeerthi/Ansible-Demo.git"
+}
