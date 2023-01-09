e8196db5cd0ec59077786c5462120d3596a51158
KrishnaPrasad K S <krishnaprasad1287@gmail.com>
2023-01-09 16:24:58 +0530

    Update ansible.groovy

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -5,3 +5,7 @@ def ping(String pathOfKey, String hosts){
 def playbook_execution(String pathOfKey, String playbook){
   sh "ansible-playbook --key-file ${pathOfKey} ${playbook}"
 }
+
+def ansible_deploy_playbook(String inventory, String playbook){
+  sh "ansible-playbook -i ${inventory} ${playbook}"
+}
