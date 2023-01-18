7fa12b582374f7c02fc7f2cc68a4bf27cc92cb00
KrishnaPrasad K S <krishnaprasad1287@gmail.com>
2023-01-18 14:21:28 +0530

    Update ansible.groovy

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -9,3 +9,7 @@ def playbook_execution(String pathOfKey, String playbook){
 def ansible_deploy_playbook(String inventory, String playbook){
   sh "ansible-playbook -i ${inventory} ${playbook}"
 }
+
+def ansible_deploy_playbook1(String inventory, String playbook, String ansible_vault){
+  ansiblePlaybook inventory: ${inventory}, playbook: ${playbook}, vaultCredentialsId: ${ansible_vault}
+}
